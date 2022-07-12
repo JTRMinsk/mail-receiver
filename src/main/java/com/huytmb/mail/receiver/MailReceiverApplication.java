6867ddb1bb7a29d9b9c8de40cb1b4d0b1ad1d686
JTRMinsk <jtrminsk@yahoo.com>
2022-07-12 22:06:11 +0800

    update comment

--- src/main/java/com/huytmb/mail/receiver/MailReceiverApplication.java
+++ src/main/java/com/huytmb/mail/receiver/MailReceiverApplication.java
@@ -3,6 +3,9 @@ package com.huytmb.mail.receiver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Admin
+ */
 @SpringBootApplication
 public class MailReceiverApplication {
 
